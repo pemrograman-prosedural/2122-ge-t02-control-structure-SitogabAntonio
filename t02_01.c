1c20b3e24d11c0834369ae18ce9cfacc7594d403
Sitogab Antonio <antonio.girsang@gmail.com>
2022-02-16 15:33:20 +0700

    t02_01

--- t02_01.c
+++ t02_01.c
@@ -5,8 +5,42 @@
 
 int main(int _argv, char **_argc)
 {
-  //code
-  
-  return 0;
+  int buku, harga, potonganharga, totalharga, dibayar;
 
+  scanf("%d", buku);
+  scanf("%d", harga);
+ 
+  totalharga = buku * harga;
+
+  if(totalharga > 500000) {
+    potonganharga = totalharga * 15 / 100;
+    dibayar = totalharga - potonganharga;
+
+    printf("%d\n", potonganharga);
+    printf("%d\n", dibayar);
+  } else  {
+    if (totalharga >= 100000)  {
+      potonganharga = totalharga * 10 / 100;
+
+      printf("%d\n", potonganharga);
+      printf("%d\n", dibayar);
+    } else  {
+      if (totalharga > 50000) {
+        potonganharga = totalharga * 5 / 100;
+        dibayar = totalharga - potonganharga;
+
+        printf("%d\n", potonganharga);
+        printf("%d\n", dibayar);
+    } else  {
+      if(totalharga < 50000)  {
+        potonganharga = 0;
+        dibayar = totalharga - potonganharga;
+        printf("%s\n", "---");
+        printf("%d\n", dibayar);
+       }
+     }
+   }
+  }
+  return 0;
+ 
 }
