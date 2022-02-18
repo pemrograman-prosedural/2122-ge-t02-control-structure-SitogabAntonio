8f4cfcad1807fe42c2a61a6b57b92705bcd38692
Sitogab Antonio <antonio.girsang@gmail.com>
2022-02-18 09:22:20 +0700

    mencoba t02_02 dan t02_02

--- t02_01.c
+++ t02_01.c
@@ -6,41 +6,43 @@
 int main(int _argv, char **_argc)
 {
   int buku, harga, potonganharga, totalharga, dibayar;
+    scanf("%d", &buku);
+    scanf("%d", &harga);
 
-  scanf("%d", buku);
-  scanf("%d", harga);
- 
-  totalharga = buku * harga;
+    totalharga = buku * harga;
 
-  if(totalharga > 500000) {
-    potonganharga = totalharga * 15 / 100;
-    dibayar = totalharga - potonganharga;
+    if (totalharga > 500000)
+    {
+        potonganharga = totalharga * 15 / 100;
+        dibayar = totalharga - potonganharga;
 
-    printf("%d\n", potonganharga);
-    printf("%d\n", dibayar);
-  } else  {
-    if (totalharga >= 100000)  {
-      potonganharga = totalharga * 10 / 100;
-
-      printf("%d\n", potonganharga);
-      printf("%d\n", dibayar);
-    } else  {
-      if (totalharga > 50000) {
+        printf("%d\n", potonganharga);
+        printf("%d\n", dibayar);
+    }
+    else if (totalharga >= 100000)
+    {
+        potonganharga = totalharga * 10 / 100;
+        dibayar = totalharga - potonganharga;
+        printf("%d\n", potonganharga);
+        printf("%d\n", dibayar);
+    }
+    else if (totalharga > 50000)
+    {
         potonganharga = totalharga * 5 / 100;
         dibayar = totalharga - potonganharga;
 
         printf("%d\n", potonganharga);
         printf("%d\n", dibayar);
-    } else  {
-      if(totalharga < 50000)  {
+    }
+    else if (totalharga < 50000)
+    {
         potonganharga = 0;
         dibayar = totalharga - potonganharga;
         printf("%s\n", "---");
         printf("%d\n", dibayar);
-       }
-     }
-   }
-  }
+    }
+
+ 
   return 0;
  
 }
--- t02_02.c
+++ t02_02.c
@@ -5,7 +5,43 @@
 
 int main(int _argv, char **_argc)
 {
-  //code
+  int pilih;
+  scanf("%d", &pilih);
+  if(pilih==1){
+    printf("%s\nstaple food");
+    printf("%s\nyou need side dishes");
+  }else{
+    if(pilih==2){
+      printf("%s\nside dishes");
+      printf("%s\nstaple food");
+      printf("%s\nyou need vegetables");
+    }else{
+      if(pilih==3){
+        printf("%s\nvegetables");
+        printf("%s\nside dishes");
+        printf("%s\nstaple food");
+        printf("%s\ngood");
+      }else{
+        if(pilih==4){
+          printf("%s\nfruits");
+          printf("%s\nvegetables");
+          printf("%s\nside dishes");
+          printf("%s\nstaple food");
+          printf("%s\nvery good");
+        }else{
+          if(pilih==5){
+            printf("%s\nmilk");
+            printf("%s\nfruits");
+            printf ("%s\nvegetables");
+            printf("%s\nside dishes");
+            printf("%s\nstaple food");
+            printf("%s\nperfect");
+          }
+        }
+      }
+    }
+  } 
+  
   
   return 0;
  
